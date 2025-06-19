2237c4f0b0aa03a799b12900363ede3982785406
arbajit19 <AS001027572@TechMahindra.com>
2025-06-19 12:57:49 +0530

    merge with tailwind config with prev

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
     <>
-      
+      <h1 className="text-3xl font-bold underline">
+        Welcome to the Next.js App!
+      </h1>
+      <p className="mt-4">This is a simple page using Tailwind CSS.</p>
+      <button className="mt-4 bg-blue-900 text-white px-4 py-2 rounded">
+        Click Me
+      </button>
+      <p className="mt-4">This is a simple page using Tailwind CSS.</p>
+      <p className="mt-4">This is a simple page using Tailwind CSS.</p>
+      <p className="mt-4">This is a simple page</p>
     </>
   );
 }
